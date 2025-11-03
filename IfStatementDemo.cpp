e801ab5077c2d6603803290767d6341bf8b5b55c
vivonicoleshane <vivonicoleshane_bsit@plmun.edu.ph>
2025-11-03 17:01:08 +0800

    Create  IfStatementDemo.cpp

---
+++ IfStatementDemo.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int num;
+
+    num = 8;
+    cout << "Enter a number: " << num << endl;
+    if (num > 0)
+        cout << "The number is positive." << endl;
+
+    cout << endl; 
+    
+    num = 0;
+    cout << "Enter a number: " << num << endl;
+    if (num > 0)
+        cout << "The number is positive." << endl;
+
+    return 0;
+}
